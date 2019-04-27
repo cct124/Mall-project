c9decc3f1dea52ebd3661700fedd804acd1c17b3
Jane <727751354@qq.com>
2019-04-27 13:10:57 +0800

    前端商城框架

---
+++ Mall/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+      <svg-icon icon-class="telegram" class="icon-size"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style scoped>
+.icon-size {
+  font-size: 20px;
+}
+</style>
+
